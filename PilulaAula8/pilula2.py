83e7f775697c31c044ae88cb41f93bf2901f5241
GustavoGoularte01 <gustavogoularte9@gmail.com>
2026-04-22 22:34:30 -0300

    PilulaAula8
    PilulaAula8

---
+++ PilulaAula8/pilula2.py
@@ -0,0 +1,26 @@
+def especialidade_top(consultas):
+    cont = {}
+    for consulta in consultas:
+        esp = consulta['especialidade']
+        if esp not in cont:
+            cont[esp] = 0
+        cont[esp] +=1
+    maior = ''
+    max_valor = -1
+    for e in cont:
+        if cont[e] > max_valor:
+            max_valor = cont[e]
+            maior = e 
+    return maior
+
+def main():
+    consultas = [
+        {'paciente':'Ana'  ,'especialidade': 'cardiologia'},
+        {'paciente': 'Carlos','especialidade': 'Ortopedia'},
+        {'paciente': 'Beatriz','especialidade': 'cardiologia'},
+        {'paciente': 'Joao','especialidade': 'cardiologia'}
+    ]
+    resultado = especialidade_top(consultas)
+    print(f'Especialidade mais frequente: {resultado}')
+    
+main()
---
+++ PilulaAula8/pilula4.py
@@ -0,0 +1,13 @@
+def atualizar_historico(historico, paciente):
+    if paciente in historico:
+        historico.remove(paciente)
+    historico.append(paciente)
+    return historico
+def main():
+    historico = ['Ana', 'Guilherme', 'Denilson']
+    novo = 'Guilherme'
+    print(historico)
+    historico = atualizar_historico(historico, novo)
+    print(f'paciente retornando: {novo}')
+    print(f'Hitorico atualizado: {historico}')
+main()
